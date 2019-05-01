1df6929b566be7152e828ea1b36fcc5794992284
sajadweb <daram3118@gmail.com>
2019-05-01 18:29:48 +0430

    init and variables

---
+++ basicsyntax/variables.py
@@ -0,0 +1,19 @@
+"""
+salam this is comment
+"""
+'''
+this is comment
+print("hi")
+'''
+a = 2 # int
+b = 2.3 # float
+c = "string" # str
+d = True  # bool
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+a, b, c, d = (1, 2.2, "ssss", False)
+
+print(a, b, c, d )
