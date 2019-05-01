61bddc60b5a6fe0da8e6ae7839dfd1e35361734f
sajadweb <daram3118@gmail.com>
2019-05-01 18:36:51 +0430

    variables comment and Basic math

--- basicsyntax/variables.py
+++ basicsyntax/variables.py
@@ -5,15 +5,20 @@ salam this is comment
 this is comment
 print("hi")
 '''
+# assigment
 a = 2 # int
 b = 2.3 # float
 c = "string" # str
 d = True  # bool
+# check type
 print(type(a))
 print(type(b))
 print(type(c))
 print(type(d))
-
+# Multiple assigment
 a, b, c, d = (1, 2.2, "ssss", False)
-
 print(a, b, c, d )
+
+# Basic math
+z = a + b
+print(z, type(z))
